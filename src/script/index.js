53737c98dbf1de454cac1a8c6ec3526a809808e8
MiDDiz <xxmidone30xx@gmail.com>
2022-12-08 14:50:02 +0100

    refactor index.js, hide log-sign buttons on logged=true

--- src/script/index.js
+++ src/script/index.js
@@ -3,20 +3,11 @@ let is_sidebar_on=0;
 
 __init__();
 
-function __init__(){
-    let scr_width = screen.width
-
-    if (scr_width<="600"){
-        let sidebar = $('#id-sidebar');
-        sidebar.hide();
-    }
-}
-
 function openPage(site) {
     document.location.href = site;
 }
 
-function openMobileSidebar(){
+function openMobileSidebar() {
     let sidebar = $('#id-sidebar');
     if (is_sidebar_on==0){
         is_sidebar_on=1;
@@ -27,4 +18,34 @@ function openMobileSidebar(){
         sidebar.hide();
     }
     
+}
+
+function hide_side_bar_on_mobile() {
+    let scr_width = screen.width;
+
+	if (scr_width <= "600") {
+		let sidebar = $('#id-sidebar');
+		sidebar.hide();
+	}
+}
+
+function is_logged() {
+	if (localStorage.getItem("logged") == null)
+		return false;
+	return true;
+}
+
+function switch_header_logged(){
+	$(".sign-log-in-button").hide();
+	// TODO: Mostrar el boton del usuario logeado...
+}
+
+function __init__() {
+	/* Lógica global*/
+	hide_side_bar_on_mobile();
+	
+	/* Logica de usuario loggeado */
+	if (!is_logged())
+		return ;
+	switch_header_logged();
 }
