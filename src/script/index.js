9c99d33ed0cff663501cbeadc5e96b7e611ae182
AlvaroPV <100451185@alumnos.uc3m.es>
2022-12-08 13:38:36 +0100

    cosas

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,21 @@
 /* Index script */
+let is_sidebar_on=0;
+
 
 function openPage(site) {
     document.location.href = site;
+}
+
+function openMobileSidebar(){
+    let sidebar = $('#id-sidebar');
+    if (is_sidebar_on==0){
+        is_sidebar_on=1;
+        sidebar.show("100");
+        sidebar.style.display="flex";
+    }
+    else if (is_sidebar_on==1){
+        is_sidebar_on=0;
+        sidebar.hide();
+    }
+    
 }
