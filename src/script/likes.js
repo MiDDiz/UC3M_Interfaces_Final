3503389f6f650b92ef18e42dcedf03f28f3b7fb4
pablo <100429084@alumnos.uc3m.es>
2022-12-15 13:49:55 +0100

    me gusta perfil añadido (fallos en el reproductor)

---
+++ src/script/likes.js
@@ -0,0 +1,25 @@
+/*News script*/ 
+
+__init__();
+
+function __init__() {
+    let User = new UserData();
+    User.getCookie();
+    let likes = User.liked_songs;
+    let i=0;
+    while (i<likes.length){
+        console.log(likes.length);
+        console.log(likes[i].path);
+        $(".likes-song").append(`
+            <div class="rand-song">
+                <a class="round-button" onclick = "staticChangeSong(${likes[i].cover},${likes[i].title},${likes[i].artist},${likes[i].path})"><i class="fa fa-play fa-2x"></i></a>
+                <div class="cover-img">
+                    <img src= ${likes[i].cover}>
+                    <p class="title">${likes[i].title}</p>
+                    <p class="artist">${likes[i].artist}</p>	
+                </div>
+            </div>
+        `)
+        i++
+}
+}
